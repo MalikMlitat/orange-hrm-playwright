d57fb489453784f4bc661856bc0d77402d62bd45
Ahmad Hanani <ahmadj7hanani0@gmail.com>
2026-02-26 04:17:06 +0200

    Add employee test

---
+++ tests/test_add_employee.py
@@ -0,0 +1,26 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_example(page: Page) -> None:
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("admin")
+    page.get_by_role("textbox", name="Password").click()
+    page.get_by_role("textbox", name="Password").fill("admin123")
+    page.get_by_role("button", name="Login").click()
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
+    expect(page.get_by_role("heading")).to_contain_text("Dashboard")
+    page.get_by_role("link", name="PIM").click()
+    page.get_by_role("button", name=" Add").click()
+    page.get_by_role("textbox", name="First Name").click()
+    page.get_by_role("textbox", name="First Name").fill("ahmad")
+    page.get_by_role("textbox", name="Middle Name").click()
+    page.get_by_role("textbox", name="Middle Name").fill("nahi")
+    page.get_by_role("textbox", name="Last Name").click()
+    page.get_by_role("textbox", name="Last Name").fill("hanani")
+    page.get_by_role("button", name="Save").click()
+    expect(page.locator("#oxd-toaster_1")).to_contain_text("Success")
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/210")
+    expect(page.locator("#app")).to_contain_text("Personal Details")
+# updated
